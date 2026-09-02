c854385bc80a1b70461133e971dd77c0ade9ff87
pedropontieri-prog <pedro.pontieri@aluno.ifsp.edu.br>
2026-09-02 20:59:22 -0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,17 +1,21 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseUrl =
+  import.meta.env.VITE_SUPABASE_URL;
+
 const supabasePublishableKey =
-  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 if (!supabaseUrl) {
-  throw new Error('VITE_SUPABASE_URL não configurada')
+  throw new Error(
+    'VITE_SUPABASE_URL não configurada'
+  );
 }
 
 if (!supabasePublishableKey) {
   throw new Error(
     'VITE_SUPABASE_PUBLISHABLE_KEY não configurada'
-  )
+  );
 }
 
 export const supabase = createClient(
@@ -25,4 +29,4 @@ export const supabase = createClient(
       storage: window.localStorage,
     },
   }
-)
+);
